131493a3541b7d858749c4ec6269de5db835e5cc
sho1cchi <ichikawa.s.417@gmail.com>
2019-07-13 18:29:22 +0900

    データ操作の許可を追加

--- develop/app/controllers/application_controller.rb
+++ develop/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:address_id, :family_name, :first_name, :kana_family_name, :kana_first_name, :phone_number, :is_deleted])
+	end
 end

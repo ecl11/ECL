79f18795394c76d8c98d417c284b7d2efc808a74
sho1cchi <ichikawa.s.417@gmail.com>
2019-07-14 19:55:06 +0900

    enum下準備

--- develop/app/models/order.rb
+++ develop/app/models/order.rb
@@ -2,4 +2,17 @@ class Order < ApplicationRecord
 
 	has_many :order_items, dependent: :destroy
 	belongs_to :user
+
+	enum payment_method: {
+		bank_transfer: 0,
+		cod: 1,
+		credit_card_payment: 2
+	}
+
+	enum delivery_status: {
+		reception: 0,
+		preparing_product: 1,
+		shipped: 2
+	}
+
 end
--- develop/config/application.rb
+++ develop/config/application.rb
@@ -10,6 +10,7 @@ module ECL
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    #enumの日本語化に必要ではない？？NoMethodError: undefined method `default_local=' for I18n:Moduleが出る　＝＞　config.i18n.default_local = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

a19bc93ee0c807030eb6999dc8f77e22122387da
shoicchi <51253545+shoicchi@users.noreply.github.com>
2019-07-28 14:24:11 +0900

    Update items_controller.rb

--- develop/app/controllers/users/items_controller.rb
+++ develop/app/controllers/users/items_controller.rb
@@ -24,26 +24,6 @@ def show
 
 
 
-
-
-=begin
-  def show
-  	@item = Item.find(params[:id])
-    @discs = @item.discs.all
-    @user = current_user
-    @cart_items = @user.cart_items
-
-    @current_item = @user.cart_items.find_by(item_id: @item.id)
-
-    if  @current_item
-      @cart_item = CartItem.find(@current_item.id)
-    else
-      @cart_item = CartItem.new
-    end
-  end
-=end
-
-
   private
   def item_params
     params.require(:item).permit(:product_name, :price, :jacket_image, :remove_jacket_image, :stock_quantity, :sales_status, discs_attributes: [:id, :disc_name, :_destroy,

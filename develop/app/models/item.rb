f3137dd60e7e3d059f706eaf600631bbbd0fce15
jinszk <jinszk1991@gmail.com>
2019-07-28 15:50:40 +0900

    途中経過

--- develop/app/models/item.rb
+++ develop/app/models/item.rb
@@ -3,9 +3,9 @@ class Item < ApplicationRecord
 	acts_as_paranoid
 
 	has_many :discs, :dependent => :destroy
-	accepts_nested_attributes_for :discs, allow_destroy: true
+	has_many :order_items, :dependent => :destroy
+  accepts_nested_attributes_for :discs, allow_destroy: true
 
-	belongs_to :order_item, optional: true
 	belongs_to :artist, optional: true
 	belongs_to :label, optional: true
 	belongs_to :genre, optional: true

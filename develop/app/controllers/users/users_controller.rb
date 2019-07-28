9412f5621a5d26706e5fcc0ab793121facdf4f28
kouaaaunun <shirley_junjun@yahoo.co.jp>
2019-07-28 17:19:27 +0900

    最新

--- develop/app/controllers/users/users_controller.rb
+++ develop/app/controllers/users/users_controller.rb
@@ -6,7 +6,6 @@ class Users::UsersController < ApplicationController
     @user = current_user
     @current_address = @user.addresses.first
     if @current_address #@user.ddresses.present?
-      @address = Address.find_by(post_number: params[:post_number],address: params[:address])
 
       @orders = Order.where(user_id: current_user.id).page(params[:page]).per(3)
       @items = Item.all#
@@ -23,17 +22,18 @@ class Users::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    if params[:user][:encrypted_password].blank?
+      params[:user].delete("encrypted_password")
+    end
     if @user.update(user_params)
       redirect_to users_user_path(@user.id)
-    else
-      end
+    end
   end
 
 
   private
   def user_params
-  params.require(:user).permit(:email,:encrypted_password,:address_id, :family_name,:firstname,:kana_family_name,:kana_first_name,:phone_number,
-                                address_attributes:[:id,:post_number,:address])
+  params.require(:user).permit(:email,:encrypted_password,:address_id, :family_name,:firstname,:kana_family_name,:kana_first_name,:phone_number)
   end
   def address_params
     params.require(:address).permit(:post_number,:address)
